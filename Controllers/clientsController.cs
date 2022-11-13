7c0d79e1ba4d981d22ce701a91c4ba5b151db65b
esraa267 <esraa.mohamed.fcis@gmail.com>
2022-11-13 10:42:46 +0200

    first commit

---
+++ Controllers/clientsController.cs
@@ -0,0 +1,66 @@
+﻿using CRUD_Api.Models;
+using CRUD_Api.services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Api.Controllers
+{
+  
+    [Route("api/[controller]")]
+    [ApiController]
+    public class clientsController :ControllerBase
+    {
+        [HttpGet]
+        public  ActionResult<List<clients>> Get()
+        {
+           
+            return Ok(clientServise.GetAll());
+        }
+        [HttpGet("{id}")]
+        public ActionResult<List<clients>> Get(int id)
+        {
+            var res = clientServise.GetById(id);
+            if (res == null)
+            {
+                return BadRequest("client not found");
+            }
+            return Ok(res);
+        }
+        [HttpPost]
+        public ActionResult<List<clients>> Add(clients data)
+            
+        {
+            clientServise.Add(data);
+
+
+            return Ok(data);
+        }
+
+        [HttpPut]
+        public ActionResult<List<clients>>Update(clients data)
+
+
+        {
+            var res = clientServise.Update(data);
+            if (res == null)
+            {
+                return BadRequest("client not found");
+            }
+        
+            return Ok(data);
+        }
+        [HttpDelete("{id}")]
+        public ActionResult<List<clients>> Delete(int id)
+
+
+        {
+            var res =  clientServise.Delete(id);
+            if (res == null)
+            {
+                return BadRequest("client not found");
+            }
+
+   
+            return Ok(res);
+        }
+    }
+}
---
+++ Models/clients.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_Api.Models
+{
+    public class clients
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Place { get; set; } 
+    }
+}
